7f435ec5e1206a68e9c5b232b955dd3d95147c6e
Mihail <mihail.bashkatov@gmail.com>
2023-08-04 14:19:42 +0200

    Added next methods: 1) Class method instantiate_from_csv, 2)Static method string_to_number, 3) Setter and getter for name, 4) Name converted into private attribute

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-
 
     def calculate_total_price(self) -> float:
         """
@@ -27,7 +29,6 @@ class Item:
 
         return self.price * self.quantity
 
-
     def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
@@ -35,3 +36,31 @@ class Item:
 
         self.price *= self.pay_rate
         return self.price
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) > 10:
+            self.__name = value[0:10]
+        else:
+            self.__name = value
+
+    @staticmethod
+    def string_to_number(string):
+        if '.' in string:
+            return int(float(string))
+        else:
+            return int(string)
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                for value in row.values():
+                    value_decoded = value.encode('ISO-8859-1').decode('cp1251')
+                    value_new = value_decoded.split()
+                    cls.all.append(cls(value_new[0], value_new[1], value_new[2]))
