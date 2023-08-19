31d719dd2d6d93707da40b19d8d361e33cafbfcc
Mihail <mihail.bashkatov@gmail.com>
2023-08-19 22:32:13 +0200

    Added methods __add__ and __radd__, Added static method get_number_of_sim, used for check value number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -9,22 +9,40 @@ class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
 
-        # Если количество сим-карт меньше одной, то вызывается ошибка и выводится сообщение пользователю
-        if number_of_sim < 1:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
+        # Проверка на количество number_of_sim больше 0
+        self.get_number_of_sim(number_of_sim)
         self.__number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"{__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
+    def __add__(self, other):
+        """ Метод позволяет складывать только экземпляры класса Phone and Item,
+        где первое слагаемое - экземпляр класса Phone"""
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        else:
+            return 'Второе слагаемое не является атрибутом класса Item'
+
+    def __radd__(self, other):
+        """ Метод позволяет складывать только экземпляры класса Phone and Item,
+                где второе слагаемое - экземпляр класса Phone"""
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        else:
+            return 'Первое слагаемое не является атрибутом класса Item'
+
     @property
     def number_of_sim(self):
         return self.__number_of_sim
 
-    @number_of_sim.setter
-    def number_of_sim(self, value):
-
-        # Если количество сим-карт меньше одной, то вызывается ошибка и выводится сообщение пользователю
-        if value < 1:
+    @staticmethod
+    def get_number_of_sim(number):
+        """ Проверка на количество number_of_sim больше 0"""
+        if number < 1:
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
-        self.__number_of_sim = value
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        self.get_number_of_sim(number_of_sim)
+        self.__number_of_sim = number_of_sim
