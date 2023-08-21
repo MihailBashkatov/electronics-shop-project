25f39173aaa622c6318aaf691f3a530c34635991
Mihail <mihail.bashkatov@gmail.com>
2023-08-21 07:46:42 +0200

    __add__ method was added

--- src/item.py
+++ src/item.py
@@ -26,6 +26,13 @@ class Item:
     def __str__(self):
         return f'{self.name}'
 
+
+    def __add__(self, other):
+        """ Метод позволяет складывать только экземпляры класса,
+        где первое слагаемое - экземпляр класса Phone"""
+        return self.quantity + other.quantity
+
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
