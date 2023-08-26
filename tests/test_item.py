c310cc4c49bd284f59649e7d6f7bfc2eec7d0dda
Mihail <mihail.bashkatov@gmail.com>
2023-08-26 14:39:51 +0200

    Added tests in case file is abscent or damaged

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,14 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
 
+from src.InstantiateCSVError import InstantiateCSVError
 from src.item import Item
-from src.phone import Phone
 
 
 def test_calculate_total_price():
     item = Item('toy', 10.5, 3)
     assert item.calculate_total_price() == 31.5
     assert type(item.calculate_total_price()) == float
+
 
 def test_apply_discount():
     item = Item('toy', 10, 3)
@@ -18,11 +18,12 @@ def test_apply_discount():
 
 
 def test_name():
-    item = Item('toy',10, 3)
+    item = Item('toy', 10, 3)
     item.name = 'water'
     assert item.name == 'water'
     item.name = 'crocodile_green'
     assert item.name == 'crocodile_'
+
 
 def test_string_to_number():
     item = Item('toy', 10, 3)
@@ -31,12 +32,25 @@ def test_string_to_number():
     assert item.string_to_number('9.8') == 9
 
 
-def test_instantiate_from_csv():
-    Item.instantiate_from_csv()
-    assert len(Item.all) == 5
-    for item in Item.all:
-        assert repr(item)[0:4] == "Item"
-        assert repr(item)[-1] == ")"
-        for letter in str(item.name):
-            assert letter.isalpha()
+# def test_instantiate_from_csv():
+#     Item.instantiate_from_csv()
+#     assert len(Item.all) == 5
+#     for item in Item.all:
+#         assert repr(item)[0:4] == "Item"
+#         assert repr(item)[-1] == ")"
+#         for letter in str(item.name):
+#             assert letter.isalpha()
 
+
+def test_instantiate_from_csv_errors():
+    try:
+        Item.instantiate_from_csv()
+        for item in Item.all:
+            assert repr(item)[0:4] == "Item"
+            assert repr(item)[-1] == ")"
+            for letter in str(item.name):
+                assert letter.isalpha()
+    except InstantiateCSVError:
+        assert Item.instantiate_from_csv() == '_Файл item.csv поврежден_'
+    except FileNotFoundError:
+        assert Item.instantiate_from_csv() == '_Отсутствует файл item.csv_'
