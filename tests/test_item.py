4afc6dae3034499a5e6a34bd0363f1e7e4bd18d7
Mihail <mihail.bashkatov@gmail.com>
2023-08-30 15:44:52 +0200

    Deleted outdated info

--- tests/test_item.py
+++ tests/test_item.py
@@ -34,30 +34,6 @@ def test_string_to_number():
     assert item.string_to_number('1.3') == 1
     assert item.string_to_number('9.8') == 9
 
-
-# def test_instantiate_from_csv():
-#     Item.instantiate_from_csv()
-#     assert len(Item.all) == 5
-#     for item in Item.all:
-#         assert repr(item)[0:4] == "Item"
-#         assert repr(item)[-1] == ")"
-#         for letter in str(item.name):
-#             assert letter.isalpha()
-
-
-# def test_instantiate_from_csv_errors():
-#     try:
-#         Item.instantiate_from_csv()
-#         for item in Item.all:
-#             assert repr(item)[0:4] == "Item"
-#             assert repr(item)[-1] == ")"
-#             for letter in str(item.name):
-#                 assert letter.isalpha()
-#     except InstantiateCSVError:
-#         assert Item.instantiate_from_csv() == '_Файл item.csv поврежден_'
-#     except FileNotFoundError:
-#         assert Item.instantiate_from_csv() == '_Отсутствует файл item.csv_'
-
 def test_instantiate_from_csv():
     Item.instantiate_from_csv()
     for item in Item.all:
